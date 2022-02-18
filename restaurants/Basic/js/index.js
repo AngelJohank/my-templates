f67060324538d3c5106295c22c8ce2265d7ae9af
Angel <angeljohank@gmail.com>
2022-02-18 03:19:36 -0500

    finished navbar

--- restaurants/Basic/js/index.js
+++ restaurants/Basic/js/index.js
@@ -1,1 +1,10 @@
 feather.replace()
+
+// Actual code
+
+const toggleButton = document.getElementById("nav-toggle");
+const navLinks = document.querySelector(".nav-menu");
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-menu--visible");
+});
