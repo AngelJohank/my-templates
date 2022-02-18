09cf5bbe49846e58b8ae03511da3557722481ebd
Angel <angeljohank@gmail.com>
2022-02-18 03:37:34 -0500

    accesibility

--- restaurants/Basic/js/index.js
+++ restaurants/Basic/js/index.js
@@ -7,4 +7,10 @@ const navLinks = document.querySelector(".nav-menu");
 
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("nav-menu--visible");
+
+  if (navLinks.classList.contains("nav-menu--visible")) {
+    toggleButton.setAttribute("aria-label", "cerrar menú");
+  } else {
+    toggleButton.setAttribute("aria-labeñ", "abrir menú")
+  }
 });
